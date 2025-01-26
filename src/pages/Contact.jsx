5469cfcf190740c24922db4ba14c90258e202587
owakeelAhmmed <owakeel.ahmmed@gmail.com>
2025-01-26 19:54:30 +0600

    Complete login page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,29 @@
+import { assets } from "../assets/assets"
 
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <div className="text-center text-3xl pt-10">
+        <p>Contact Us</p>
+      </div>
+
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm">
+        <img className="w-full md:max-w-[360px]" src={assets.contact_image} alt="" />
+
+        <div className="flex flex-col justify-center items-start gap-6 ">
+        <p>OUR OFFICE</p>
+        <p>00000 Willms <br /> Station Suite 000, Washington, USA</p>
+        <p>Tel: (000) 000-0000 <br />
+        Email: greatstackdev@gmail.com</p>
+        <p>CAREERS AT PRESCRIPTO</p>
+        <p>Learn more about our teams and job openings.</p>
+        <button className="border px-8 py-4 cursor-pointer hover:bg-primary transition-all duration-300">Explore</button>
+      </div>
+      </div>
+
+     
+    </div>
   )
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,47 @@
+import { useState } from "react"
 
 
 const Login = () => {
+
+  const [state, setState] = useState('Sing Up');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+
+  const onSubmitHandler = async (event) => {
+      event.preventDefault();
+  }
+
+
   return (
-    <div>Login</div>
+    <form className="min-h-[80vh] flex items-center">
+      <div className="flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-sm shadow-lg">
+          <p>{state === "Sing Up" ? "Create Account" : "Login"}</p>
+          <p>Please {state === "Sing Up" ? "sing up" : "login"} to book appointment</p>
+
+          {
+            state === "Sing Up" && 
+            <div className="w-full">
+              <p>Full Name</p>
+              <input className="border border-green rounded w-full p-2 mt-1" type="text" onChange={(e) => setName(e.target.name)} value={name} required />
+            </div>
+          }
+          
+          <div className="w-full">
+              <p>Email</p>
+              <input className="border border-gray rounded w-full p-2 mt-1" type="emil" onChange={(e) => setEmail(e.target.name)} value={email} required />
+          </div>
+          <div className="w-full">
+              <p>Password</p>
+              <input className="border border-gray rounded w-full p-2 mt-1" type="password" onChange={(e) => setPassword(e.target.name)} value={password} required />
+          </div>
+          <button className="button_style p-3 w-full cursor-pointer">{state === "Sing Up" ? "Create Account" : "Login"}</button>
+
+          {
+            state === "Sing Up" ? <p>Already have an account? <span onClick={() => setState('Login')} className="underline cursor-pointer text-green]">Login here</span></p> : <p>Create an new account? <span onClick={() => setState('Sing Up')} className="underline cursor-pointer text-green]">Click Here</span></p>
+          }
+      </div>
+    </form>
   )
 }
 
